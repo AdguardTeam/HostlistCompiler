abe6fa3d11584c03f38db9f0a4fe0fc3fc2b1405
Elizaveta Egorova <e.egorova@adguard.com>
2024-12-18 18:06:26 +0300

    Calculate checksum for filters and add a '! Checksum' meta to the filter list. #76
    Squashed commit of the following:
    
    commit a876ffca31c02acc80525f805f4ad58743a66ed5
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 19:20:40 2024 +0300
    
        add test for calculateChecksum
    
    commit c7a0b15f4f6dcd1afeb50baa2fdab8cf8f246feb
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 17:45:29 2024 +0300
    
        fix indents
    
    commit 3bdaa4873b1bdcecec422df3d5ff02eeff25a0a5
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 17:40:12 2024 +0300
    
        add test to check checksum value
    
    commit 2e6d05e54ac83fc414c6cafb63c28f12d892ec12
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 16:59:09 2024 +0300
    
        update changelog
    
    commit 44788ec4124fa7795f8364ed044fa8b0001bfb42
    Merge: efbff9b 82247f9
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 16:56:14 2024 +0300
    
        Merge branch 'fix/#76_add_checksum' of ssh://bit.int.agrd.dev:7999/adguard-filters/hostlist-compiler into fix/#76_add_checksum
    
    commit efbff9b947db6e509af959f31ee377fddf29ea53
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 16:56:09 2024 +0300
    
        fix jsDoc comments
    
    commit 82247f9cfa2361506618ac3e15fe9dc4b761cb66
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Fri Dec 13 16:55:45 2024 +0300
    
        Applied suggestion
    
    commit 4ab778fcf4706f82f59c5f19f0958c2d2811c929
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Dec 13 16:41:48 2024 +0300
    
        Calculate checksum for filters and add a '! Checksum' meta to the filter list. #76

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const nock = require('nock');
 const consola = require('consola');
 const compile = require('../src/index');
+const { calculateChecksum } = require('../src/utils');
 
 describe('Hostlist compiler', () => {
     it('compile from multiple sources', async () => {
@@ -32,12 +33,48 @@ describe('Hostlist compiler', () => {
         const list = await compile(configuration);
 
         // assert
+        expect(list[1].startsWith('! Checksum:')).toBe(true);
         expect(list).toContain('||example.org');
         expect(list).toContain('||example.com');
         expect(list).toContain('! Version: 1.0.0.9');
 
         const str = list.join('\n');
         consola.info(str);
+
+        scope.done();
+    });
+
+    it('calculates checksum correctly', async () => {
+        // Prepare source
+        const scope = nock('https://example.org')
+            .get('/source1.txt')
+            .reply(200, '||example.org');
+
+        // compiler configuration
+        const configuration = {
+            name: 'Test filter',
+            description: 'Checksum test filter',
+            version: '1.0.0.1',
+            sources: [
+                {
+                    name: 'source 1',
+                    source: 'https://example.org/source1.txt',
+                },
+            ],
+        };
+
+        // compile the final list
+        const list = await compile(configuration);
+
+        // assert
+        const checksumLine = list[1];
+        expect(checksumLine.startsWith('! Checksum:')).toBe(true);
+
+        const header = list.slice(2, list.indexOf('||example.org'));
+        const finalList = list.slice(list.indexOf('||example.org'));
+
+        const expectedChecksum = calculateChecksum(header, finalList);
+        expect(checksumLine).toBe(expectedChecksum);
 
         scope.done();
     });
